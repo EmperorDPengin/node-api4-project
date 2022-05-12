d24ab0e8c4180afb8a18633c4d6d29418b733f8d
Emperor D. Pengin <emperord.pengin@gmail.com>
2022-05-12 18:31:00 -0400

    fixed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require("./api/server");
 
-const PORT = process.env.PORT_ENV || 8000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
